dc59a45f4386aa7945dbb6a6d24d66f9f8d14b1e
Cris March <crmperocillo@proweaver.net>
2023-06-01 17:45:03 +0800

    Add tinyurl_api.php file

---
+++ tinyurl_api.php
@@ -0,0 +1,9 @@
+<?php
+echo file_get_contents('http://tinyurl.com/api-create.php?url='.'http://www.example.com/');
+
+/* For example
+http://tinyurl.com/api-create.php?url=https://crismarchperocillo.github.io/Portfolio/
+Would return:
+https://tinyurl.com/2e85haxs
+*/
+?>
